7c6f716d19762f570ad74c586030f122694a0f38
Iaroslav Fedorov <fed.kwest@gmail.com>
2024-12-14 14:45:27 +0400

    HHC-20: detekt issues fixed.

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,12 +1,11 @@
 package ru.practicum.android.diploma.ui.search
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
@@ -38,7 +37,7 @@ class SearchFragment : Fragment() {
 
         binding.searchField.requestFocus()
 
-       binding.searchField.doOnTextChanged { text, _, _, _ ->
+        binding.searchField.doOnTextChanged { text, _, _, _ ->
             text?.let {
                 viewModel.search(text.toString())
             }
