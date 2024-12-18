6529ebb46649abcf76f3843c1b3dcf4714e46ff3
Constantine Kim <electro.passion92@gmail.com>
2024-12-18 22:56:30 +0900

    HHC-27 Добавлена функция добавления в БД

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
@@ -1,4 +1,10 @@
 package ru.practicum.android.diploma.data.db.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import ru.practicum.android.diploma.data.db.model.VacancyEntityDB
+
 interface VacancyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVacancy(vacancy: VacancyEntityDB)
 }
