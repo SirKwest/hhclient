2a5722a0ad7ba3aa9e0a3496ba35d55d1d603d06
Constantine Kim <electro.passion92@gmail.com>
2024-12-18 23:11:37 +0900

    HHC-27 Добавлена функция получения всех избранных вакансий

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
@@ -3,6 +3,8 @@ package ru.practicum.android.diploma.data.db.dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.db.model.VacancyEntityDB
 
 interface VacancyDao {
@@ -11,4 +13,7 @@ interface VacancyDao {
 
     @Delete(entity = VacancyEntityDB::class)
     suspend fun removeVacancy(vacancy: VacancyEntityDB)
+
+    @Query("SELECT * from vacancy_table")
+    fun getVacancies(): Flow<List<VacancyEntityDB>>
 }
