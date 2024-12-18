3dff1027041d89f6ee34ccad5798dfa592b2a285
Constantine Kim <electro.passion92@gmail.com>
2024-12-18 22:58:06 +0900

    HHC-27 Добавлена функция удаления из БД

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.data.db.dao
 
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import ru.practicum.android.diploma.data.db.model.VacancyEntityDB
@@ -7,4 +8,7 @@ import ru.practicum.android.diploma.data.db.model.VacancyEntityDB
 interface VacancyDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertVacancy(vacancy: VacancyEntityDB)
+
+    @Delete(entity = VacancyEntityDB::class)
+    suspend fun removeVacancy(vacancy: VacancyEntityDB)
 }
