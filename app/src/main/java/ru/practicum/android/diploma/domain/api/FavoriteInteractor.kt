6e63a9c8a143959c36b5236ed36486341afd907e
Constantine Kim <electro.passion92@gmail.com>
2024-12-19 00:00:12 +0900

    HHC-27 Добавлены абстрактные функции интерактора

--- app/src/main/java/ru/practicum/android/diploma/domain/api/FavoriteInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FavoriteInteractor.kt
@@ -1,4 +1,10 @@
 package ru.practicum.android.diploma.domain.api
 
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Vacancy
+
 interface FavoriteInteractor {
+    suspend fun insertVacancyToFavorite(vacancy: Vacancy)
+    suspend fun removeVacancyFromFavorite(vacancy: Vacancy)
+    suspend fun getFavoriteVacancies(): Flow<List<Vacancy>>
 }
