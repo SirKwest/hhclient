451073954451c625a33c6a6a2ea5c605d172f280
AFxAster <afxaster@yandex.ru>
2024-12-19 20:30:36 +0300

    HHC-56: Added mapper from network dto to a domain model

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/MapperExtensions.kt
@@ -0,0 +1,37 @@
+package ru.practicum.android.diploma.data.dto
+
+import ru.practicum.android.diploma.data.dto.response.VacancyByIdResponse
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.VacancyShort
+
+fun VacancyShortDto.toVacancyShort(): VacancyShort {
+    return VacancyShort(
+        id = id,
+        name = name,
+        location = area.name,
+        employer = employer.name,
+        logo = employer.logo?.original ?: (employer.logo?.big ?: employer.logo?.small).orEmpty(),
+        salaryLow = salary?.low,
+        salaryHigh = salary?.high,
+        currency = salary?.currency,
+    )
+}
+
+fun VacancyByIdResponse.toVacancy(): Vacancy {
+    return Vacancy(
+        id = id,
+        name = name,
+        area = area.name,
+        employer = employer.name,
+        logo = employer.logo?.original ?: (employer.logo?.big ?: employer.logo?.small),
+        salaryLow = salary?.low,
+        salaryHigh = salary?.high,
+        currency = salary?.currency,
+        keySkills = keySkills.map { it.name },
+        description = description,
+        experience = experience?.name,
+        employment = employment?.name,
+        schedule = schedule?.name,
+        url = url
+    )
+}
