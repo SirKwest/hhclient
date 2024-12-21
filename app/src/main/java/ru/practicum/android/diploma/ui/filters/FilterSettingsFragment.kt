753f7778b6ca2ed0f1fcee8dd4e9b1915de14feb
mdorogov <dorogovmax@yandex.ru>
2024-12-21 20:22:51 +0300

    HHC-33: create changing expected salary text view state

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FilterSettingsFragment.kt
@@ -1,10 +1,13 @@
 package ru.practicum.android.diploma.ui.filters
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
 
 class FilterSettingsFragment : Fragment() {
@@ -15,4 +18,22 @@ class FilterSettingsFragment : Fragment() {
         _binding = FragmentFilterSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    fun changeExpectedSalaryTextViewState(isSalaryEntered: Boolean, isSalaryEnteringFinished: Boolean) {
+        if (isSalaryEntered) {
+            binding.filterExpectedSalaryTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue))
+            if (isSalaryEnteringFinished) {
+                binding.filterExpectedSalaryTextView.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.black
+                    )
+                )
+            }
+        } else {
+            binding.filterExpectedSalaryTextView.setTextAppearance(R.style.FilterSmallerTextStyle)
+        }
+    }
+
+
 }
