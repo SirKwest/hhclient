38747f443118a15e1a57627881438928911e3778
Constantine Kim <electro.passion92@gmail.com>
2024-12-23 03:52:00 +0900

    HHC-34 Добавлен интерактор для SharedPreferences

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FilterInteractor.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.domain.api
+
+import ru.practicum.android.diploma.domain.models.Filter
+
+interface FilterInteractor {
+    fun saveFilter(filter: Filter)
+    fun getFilter(): Filter
+}
