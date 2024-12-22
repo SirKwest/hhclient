e1da9687b1b6ad45714410f93ce3c16131214fcb
Constantine Kim <electro.passion92@gmail.com>
2024-12-23 03:53:00 +0900

    HHC-34 Добавлен DI интерактора/репозитория SharedPreferences

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -2,9 +2,11 @@ package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.api.FavoriteInteractor
+import ru.practicum.android.diploma.domain.api.FilterInteractor
 import ru.practicum.android.diploma.domain.api.SharingInteractor
 import ru.practicum.android.diploma.domain.api.VacancyInteractor
 import ru.practicum.android.diploma.domain.impl.FavoriteInteractorImpl
+import ru.practicum.android.diploma.domain.impl.FilterInteractorImpl
 import ru.practicum.android.diploma.domain.impl.SharingInteractorImpl
 import ru.practicum.android.diploma.domain.impl.VacancyInteractorImpl
 
@@ -19,5 +21,9 @@ val interactorModule = module {
 
     single<FavoriteInteractor> {
         FavoriteInteractorImpl(get())
+    }
+
+    single<FilterInteractor> {
+        FilterInteractorImpl(get())
     }
 }
