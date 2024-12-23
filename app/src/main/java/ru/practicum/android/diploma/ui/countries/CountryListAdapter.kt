dd61406e260138e4a9e5cdb71ee10acf784f26bc
AFxAster <afxaster@yandex.ru>
2024-12-23 17:52:33 +0300

    HHC-44: Create country list adapter

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/countries/CountryListAdapter.kt
@@ -0,0 +1,30 @@
+package ru.practicum.android.diploma.ui.countries
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.databinding.CountriesListItemBinding
+import ru.practicum.android.diploma.domain.models.Country
+
+class CountryListAdapter(private val countries: List<Country>) : RecyclerView.Adapter<CountryListViewHolder>() {
+
+    var onItemClickListener: OnItemClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryListViewHolder {
+        return CountryListViewHolder(
+            CountriesListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: CountryListViewHolder, position: Int) {
+        holder.bind(countries[position])
+    }
+
+    override fun getItemCount(): Int {
+        return countries.size
+    }
+
+    fun interface OnItemClickListener {
+        fun onItemClick(id: String)
+    }
+}
