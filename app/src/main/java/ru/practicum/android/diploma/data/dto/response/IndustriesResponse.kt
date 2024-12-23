cbc8216c429ab585c46245e93dd98dd4fa281bc3
mdorogov <dorogovmax@yandex.ru>
2024-12-23 10:49:03 +0300

    HHC-33: unversioned files are uploaded

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/response/IndustriesResponse.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.data.dto.response
+
+import ru.practicum.android.diploma.data.dto.IndustryDto
+
+class IndustryResponse(
+    val items: List<IndustryDto>
+) : Response()
