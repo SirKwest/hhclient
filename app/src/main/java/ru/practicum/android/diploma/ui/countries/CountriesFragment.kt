23ecdf7721077db3ef0f94b0f4b451b92da0ea5e
AFxAster <afxaster@yandex.ru>
2024-12-23 21:41:54 +0300

    HHC-79: Adding processing states

--- app/src/main/java/ru/practicum/android/diploma/ui/countries/CountriesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/countries/CountriesFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentCountriesBinding
 import ru.practicum.android.diploma.domain.models.Country
@@ -27,6 +30,9 @@ class CountriesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
         viewModel.observeScreenState().observe(viewLifecycleOwner, ::processState)
     }
 
@@ -40,19 +46,52 @@ class CountriesFragment : Fragment() {
     }
 
     private fun showResults(countries: List<Country>) {
+        binding.apply {
+            countriesRecyclerView.isVisible = true
+            countriesRecyclerView.adapter = CountryListAdapter(countries).apply {
+                onItemClickListener = CountryListAdapter.OnItemClickListener {
+                    Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                }
+            }
 
+            noInternetStub.isVisible = false
+            serverErrorStub.isVisible = false
+            loading.isVisible = false
+
+        }
     }
 
     private fun showNoInternet() {
+        binding.apply {
+            noInternetStub.isVisible = true
 
+            countriesRecyclerView.isVisible = false
+            serverErrorStub.isVisible = false
+            loading.isVisible = false
+
+        }
     }
 
     private fun showRequestInProgress() {
+        binding.apply {
+            loading.isVisible = true
 
+            noInternetStub.isVisible = false
+            countriesRecyclerView.isVisible = false
+            serverErrorStub.isVisible = false
+
+        }
     }
 
     private fun showServerError() {
+        binding.apply {
+            serverErrorStub.isVisible = true
 
+            countriesRecyclerView.isVisible = false
+            noInternetStub.isVisible = false
+            loading.isVisible = false
+
+        }
     }
 
     override fun onDestroyView() {
