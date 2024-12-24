84b75b6f3f8446cc3d189b52f1b6f2e841fde704
Constantine Kim <electro.passion92@gmail.com>
2024-12-24 12:44:41 +0900

    HHC-34 Добавлены реализации сохранения/удаления фильтра

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FilterSettingsFragment.kt
@@ -13,11 +13,16 @@ import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
+import ru.practicum.android.diploma.domain.models.Filter
+import ru.practicum.android.diploma.presentation.FilterSettingsFragmentState
+import ru.practicum.android.diploma.presentation.FilterSettingsViewModel
 
 class FilterSettingsFragment : Fragment() {
     private var _binding: FragmentFilterSettingsBinding? = null
+    private val viewModel: FilterSettingsViewModel by viewModel()
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,6 +32,14 @@ class FilterSettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.observeScreenState().observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is FilterSettingsFragmentState.SavedFilter -> setPreviousFilters(state.filter)
+            }
+        }
+
+        viewModel.getFilter()
+
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
@@ -34,6 +47,17 @@ class FilterSettingsFragment : Fragment() {
             binding.onlyWithSalaryTv.toggle()
         }
         binding.applyBtn.setOnClickListener {
+            val salary =
+                if (binding.salaryEt.text.toString().isBlank()) null
+                else binding.salaryEt.text.toString().toInt()
+
+            val filter = Filter(
+                binding.locationEt.text.toString(),
+                binding.industryEt.text.toString(),
+                salary,
+                binding.onlyWithSalaryTv.isChecked
+            )
+            viewModel.saveFilter(filter)
             findNavController().navigateUp()
         }
 
@@ -90,15 +114,18 @@ class FilterSettingsFragment : Fragment() {
         }
 
         binding.resetBtn.setOnClickListener {
-            binding.locationEt.setText("")
-            binding.locationTil.endIconDrawable =
-                AppCompatResources.getDrawable(requireContext(), R.drawable.icon_arrow_forward)
-            binding.industryEt.setText("")
-            binding.industryTil.endIconDrawable =
-                AppCompatResources.getDrawable(requireContext(), R.drawable.icon_arrow_forward)
-            binding.resetBtn.isVisible = false
-            binding.applyBtn.isVisible = false
+            viewModel.saveFilter(Filter(null, null, null, false))
+            findNavController().navigateUp()
         }
     }
 
+    private fun setPreviousFilters(filter: Filter?) {
+        if (filter != null) {
+            val salary = if (filter.salary == null) "" else filter.salary.toString()
+            binding.locationEt.setText(filter.workPlace)
+            binding.industryEt.setText(filter.industry)
+            binding.salaryEt.setText(salary)
+            binding.onlyWithSalaryTv.isChecked = filter.isExistSalary
+        }
+    }
 }
