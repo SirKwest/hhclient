9db91b6c417569ec19211e2befbcb14e039a98ea
AFxAster <afxaster@yandex.ru>
2024-12-26 00:08:32 +0300

    HHC-46: Add regions search filter

--- app/src/main/java/ru/practicum/android/diploma/ui/regions/RegionsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/regions/RegionsFragment.kt
@@ -1,14 +1,21 @@
 package ru.practicum.android.diploma.ui.regions
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentRegionsBinding
 import ru.practicum.android.diploma.domain.models.Country
 import ru.practicum.android.diploma.domain.models.Region
@@ -45,7 +52,41 @@ class RegionsFragment : Fragment() {
             }
             regionsRecyclerView.adapter = regionsListAdapter
         }
+        initSearchEditText()
         viewModel.observeScreenState().observe(viewLifecycleOwner, ::processState)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun initSearchEditText() {
+        binding.searchEditText.doOnTextChanged { text, _, _, _ ->
+            viewModel.filter(text?.toString().orEmpty())
+            val drawableEnd: Drawable? = if (text?.isNotBlank() == true) {
+                ContextCompat.getDrawable(requireContext(), R.drawable.icon_delete)
+            } else {
+                ContextCompat.getDrawable(requireContext(), R.drawable.icon_search)
+            }
+            binding.searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                null,
+                null,
+                drawableEnd,
+                null
+            )
+        }
+        binding.searchEditText.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawableEnd = binding.searchEditText.compoundDrawablesRelative[2]
+                val position = binding.searchEditText.width -
+                    binding.searchEditText.paddingEnd - drawableEnd.bounds.width()
+                if (drawableEnd != null && event.rawX >= position) {
+                    binding.searchEditText.text?.clear()
+                    true
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        }
     }
 
     private fun processState(state: RegionsFragmentState) {
@@ -59,6 +100,7 @@ class RegionsFragment : Fragment() {
     }
 
     private fun showResults(regions: List<Region>) {
+        Log.d("my", "showResults $regions")
         binding.apply {
             regionsRecyclerView.isVisible = true
             content.isVisible = true
