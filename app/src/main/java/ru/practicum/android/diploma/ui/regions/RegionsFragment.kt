da789357ce57d954d0f7ef4e69b32af3a4ed1e8b
AFxAster <afxaster@yandex.ru>
2024-12-25 22:18:40 +0300

    HHC-45: Create regions layout

--- app/src/main/java/ru/practicum/android/diploma/ui/regions/RegionsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/regions/RegionsFragment.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentRegionsBinding
 import ru.practicum.android.diploma.domain.api.LocationInteractor
 import ru.practicum.android.diploma.domain.models.Country
-import ru.practicum.android.diploma.domain.models.RegionsResource
 import ru.practicum.android.diploma.ui.location.WorkLocationFragment
 import ru.practicum.android.diploma.util.getSerializableData
 
 class RegionsFragment : Fragment() {
+    private var _binding: FragmentRegionsBinding? = null
+    private val binding = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_regions, container, false)
+    ): View {
+        _binding = FragmentRegionsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,11 +37,13 @@ class RegionsFragment : Fragment() {
         lifecycleScope.launch {
             if (selectedCountryId.isNotBlank()) {
                 locationInteractor.getRegions(selectedCountryId).collect {
-                    if (it is RegionsResource.Success) {
-                        requireActivity().findViewById<TextView>(R.id.tv).text = it.items.toString()
-                    }
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
